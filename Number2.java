664f6a4516491920b0fd289332cafdf2a54efc00
ABSL-118 <73323375+ABSL-118@users.noreply.github.com>
2021-06-02 10:10:33 +0800

    Add files via upload

---
+++ Number2.java
@@ -0,0 +1,15 @@
+package progcompe;
+
+import java.util.Scanner;
+
+public class Number2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
